942e687f4577f2efc396c457001890acef340b26
Luis Alberto Santana <lsantana@credijusto.com>
2019-06-11 15:03:51 -0500

    adds initial setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+
+with open("README.rst", "r") as f:
+    long_description = f.read()
+
+
+setuptools.setup(
+    name="mx",
+    version="0.0.1",
+    author="",
+    description="Mexico state meta information and other fun stuff",
+    long_description=long_description,
+    long_description_content_type="text/restructuredtext",
+    url="",
+    packages=setuptools.find_packages(exclude=('tests', 'docs')),
+    install_requires=[],
+)
